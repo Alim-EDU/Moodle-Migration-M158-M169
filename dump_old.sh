6a5aec9c5710dd54c248253c43d41f184fd36ebc
Alim-EDU <alim.krasic@edu.gbssg.ch>
2026-05-27 21:37:19 +0200

    Config Files

---
+++ dump_old.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+# Datenbankzugangsdaten 
+MYSQL_USER="debian-sys-maint" 
+MYSQL_PASSWORD="vaIdfgRPSXzKbPPd" 
+MYSQL_DATABASE="?" 
+# Verzeichnis, in dem die Sicherungsdatei gespeichert wird 
+BACKUP_DIR="/home/vmadmin" 
+# Dateinamen für die Sicherungsdatei 
+BACKUP_FILENAME="$MYSQL_DATABASE-$(date +%Y-%m-%d_%H-%M-%S).sql" 
+# MySQL-Dump-Befehl 
+mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > $BACKUP_DIR/$BACKUP_FILENAME 
+# Erfolgsmeldung 
+echo "MySQL-Dump erfolgreich erstellt: $BACKUP_FILENAME"
